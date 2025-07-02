21ebf47b138ba722ae7c84eea4ba9d3f5592a9b0
0utplay <aldin@sijamhodzic.de>
2025-07-02 21:23:53 +0200

    Initial commit

---
+++ src/test/java/dev/playo/room/RoomManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.playo.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
